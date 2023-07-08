7ad74979e1ab7e3f3a98ea9c2173316129dee6a7
Amil Musaevi <amilmusaevi00@gmail.com>
2023-07-08 22:18:49 +0400

    add mouse events task

---
+++ jsTask/mouseEventsTask/main.js
@@ -0,0 +1,22 @@
+
+let imgCntnr1 = document.querySelector('.cntnr1')
+let imgCntnr2 = document.querySelector('.cntnr2')
+let imgCntnr3 = document.querySelector('.cntnr3')
+
+imgCntnr1.addEventListener('mouseover',()=>{
+    imgCntnr1.classList.remove("active")
+    imgCntnr2.classList.add("active")
+    imgCntnr3.classList.add("active")
+    })
+imgCntnr2.addEventListener('mouseover',()=>{
+imgCntnr2.classList.remove("active")
+imgCntnr1.classList.add("active")
+imgCntnr3.classList.add("active")
+})
+
+
+ imgCntnr3.addEventListener('mouseover',()=>{
+     imgCntnr3.classList.remove("active")
+     imgCntnr1.classList.add("active")
+     imgCntnr2.classList.add("active")
+        })
